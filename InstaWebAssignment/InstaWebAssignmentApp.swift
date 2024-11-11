d6923e8338421a02bc47f425691ce089ccda7ac9
Ujjwal Arora <ukarora9@gmail.com>
2024-11-11 23:40:34 +0530

    Initial Commit

---
+++ InstaWebAssignment/InstaWebAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaWebAssignmentApp.swift
+//  InstaWebAssignment
+//
+//  Created by Ujjwal Arora on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaWebAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
